36fe96dd5ed65abf21deceaf552d52e0f417ae62
El-Austa Rami <relausta@ozon.ru>
2021-02-17 19:03:29 +0300

    lesson 24;

---
+++ components/catalog/categories.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>categories component</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'CatalogCategories'
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>header</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Header',
+})
+</script>
+
+<style scoped>
+</style>
